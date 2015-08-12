5c2127d72a059efdfa3c7ca80f45bf6e73fc0a4c
Michael Sarfati <michael.sarfati@utoronto.ca>
2015-08-12 00:02:41 -0400

    count lines

--- 1/character_streams/count_chars2.c
+++ 1/character_streams/count_chars2.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
-int main(){
+void count_chars() {
     printf("Use ctrl+d to initiate EOF.\n");
-
     double nc;
-    for (nc = 0; getchar() != EOF; ++nc)
-        ;
+    for (nc = 0; getchar() != EOF; ++nc);
     printf("Character count: %.0f\n", nc);
+}
 
+void new_typer() {
+    int c_count = 0;
+    for(int c; ( c = getchar()) != EOF; c_count++){
+        putchar(c);
+    }
+    printf("Total characters: %d\n", c_count);
+}
+
+int main(){
+    // count_chars();
+    new_typer();
     return 0;
 }
