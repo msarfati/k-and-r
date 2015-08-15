efc0b63b7fca3e418443b9aa17fe61069c440213
Michael Sarfati <michael.sarfati@utoronto.ca>
2015-08-15 01:49:40 -0400

    exercises 1-11 and 1-12

--- 1/character_streams/word_count.c
+++ 1/character_streams/word_count.c
@@ -3,15 +3,26 @@
 #define IN  1  /* inside a word */
 #define OUT 0  /* outside a word */
 
-int main() {
-    int c, n1, nw, nc, state;
+void one_word_line() {
+    // ex-1-12 : One word per line
+    int c, state;
+    while ((c = getchar()) != EOF) {
+        if (c == *" " || c == *"\t")
+            putchar(*"\n");
+        else
+            putchar(c);
+    }
+}
+
+void word_count(){
+    int c, nl, nw, nc, state;
 
     state = OUT;
-    n1 = nw = nc = 0;
+    nl = nw = nc = 0;
     while ((c = getchar()) != EOF) {
         ++nc;
         if (c == *"\n")
-            ++n1;
+            ++nl;
         if (c == *" " || c == *"\n" || c == *"\t")
             state = OUT;
         else if (state == OUT) {
@@ -19,6 +30,15 @@ int main() {
             ++nw;
         }
     }
-    printf("%d %d %d\n", n1, nw, nc);
+    printf("%d %d %d\n", nl, nw, nc);
+}
+
+int main() {
+    one_word_line();
+    // word_count();
     return 0;
 }
+
+/*
+1-11: Numbers are counted as words, space seperators between numbers. Special characters in isolation also considered words.
+*/
